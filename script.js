f668b344df587a91b61b54508b5af9ee4046d8bd
Shaun <valensh1@yahoo.com>
2021-06-18 11:31:58 -0700

    Balance Totals for deposits_moneyOut_interest completed

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const displayMovements = (movements) => {   // Function in which the purpose is 
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${index + 1} ${type} deposit</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>`;    // Variable set up so we can use with the insertAdjacentHTML method. Notice we adjusted the class name with the type variable we created above which was the ternary operator
 
     containerMovements.insertAdjacentHTML('afterbegin', html); // Takes the containerMovements DOM variable which is just the overall container for the movements section and uses the insertAdjacentHTML method which basically inserts html right into a specified place in the DOM which in this case is the containerMovements. The first argument of the insertAdjacent HTML is where you want it inserted. The 'afterbegin' argument places the HTML after the element but before any other content that is already present inside that element. In our case with each element it loops over it places it at the beginning of the container element so finally by the last item it loops over that will be the one that shows first up top. This method is basically displaying in descending order. The other option 'beforeend' would place the items in ascending order. The 2nd argument to insertAdjacentHTML is the HTML you want to insert and in our case it is the html variable we created.
@@ -81,9 +81,26 @@ const calcDisplayBalance = (movements) => { // Function that accepts a movements
   const balance = movements.reduce((accum, current) => { // Creation of a balance variable that consists of the returned value from running the reduce method on the movements array; The method accepts an accum variable which starts at 0 unless otherwise specified at end of reduce method and then adds each current value on to the accum variable. So accum starts at 0 (unless otherwise specified) takes the first number and then adds that to the accum variable then goes to the 2nd number in array and adds that to accum variable, etc.
     return accum + current  // Add accum + current; Accum variable which starts at 0 unless otherwise specified at end of reduce method and then adds each current value on to the accum variable. So accum starts at 0 (unless otherwise specified) takes the first number and then adds that to the accum variable then goes to the 2nd number in array and adds that to accum variable, etc.
   }, 0);                    // Don't need to put 0 here as the default is 0 but if you wanted it to start at a different number you would insert a number here. For example, if you wanted 10 added to the sum of the array you would put 10 here. as the accum variable would start with 10 instead of starting at 0
-  labelBalance.textContent = `${balance} EUR`; // Adds balance inside a template literal to DOM
+  labelBalance.textContent = `${balance}€`; // Adds balance inside a template literal to DOM
 };   
 calcDisplayBalance(account1.movements); // Invoking the calcDisplayBalance method with account 1 object's movements array
+
+
+const calcDisplaySummary = (movements) => { // Function that calculates the summary balance for each account holder (summary balance for deposits/moneyOut/interest)
+  // Deposit - Summary Balance Total
+  const deposits = movements.filter(mov => mov > 0).reduce((accum, current) => accum + current); // Takes the movements array for each account holder and then filters that movements array for all amounts greater than 0 as this represents deposits or money coming in; The filter method will return a new array with all the amounts that are greater than 0. Then we use the reduce method to sum up the array that the filter method returned to get our total deposit balance.
+  labelSumIn.textContent = `${deposits}€`; // Update DOM for total deposits
+
+  // Withdrawals / Money Going Out - Summary Balance Total
+  const moneyOut = movements.filter(mov => mov < 0).reduce((accum, current) => accum + current); // Takes the movements array for each account holder and then filters that movements array for all amounts less than 0 as this represents money going out; The filter method will return a new array with all the amounts that are less than 0. Then we use the reduce method to sum up the array that the filter method returned to get our total moneyOut balance.
+  labelSumOut.textContent = `${Math.abs(moneyOut)}€`; // Update DOM for total money going out
+
+  // Interest - Interest Earned Summary Balance Total
+  const interestRate = .012; // Interest rate for our bank which is 1.2% represented in decimal format as JavaScript doesn't recognize percentages.
+  const interestAmts = movements.filter(mov => mov > 0).map(mov => mov * interestRate).filter(int => int >= 1).reduce((accum, current) => accum + current); // Takes the movements array for each account holder and then filters that movements array for all amounts greater than 0 as this represents deposits or money coming in; The filter method will return a new array with all the amounts that are greater than 0. Then we use the map method to loop over each element in the array returned from the filter method and apply the interest rate to that amount IF the interest is greater than or equal to 1. Then we use the reduce method to sum up the array that the map method returned to get our total interest balance.
+  labelSumInterest.textContent = `${interestAmts}€`; // Update DOM for total interest
+};
+calcDisplaySummary(account1.movements); // Invoking the calcDisplaySummary method with account 1 object's movements array
 
 
 const createUserNames = (accounts) => { // Function to convert account owner name to a username based on their initials; UserName will be a new key we are adding to each respective person's account object. Pass the accounts array variable which consists of [account1, account2, account3, account4] into function. Each one of these 4 accounts is a reference to an object which contains owner name, movements, interestRate & PIN
